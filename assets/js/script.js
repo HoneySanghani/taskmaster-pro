04656facb01e25964d1f10fc5c6d49ec28cf24fc
Honey Sanghani <honeysanghani98@gmail.com>
2021-07-21 17:05:50 -0400

    drag and drop created for tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,7 +44,68 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll:false,
+  tolerance:"pointer",
+  helper:"clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over", event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  
+  update: function() {
+    var tempArr = [];
+    $(this).children().each(function(){
+      var text = $(this)
+      .find("p")
+      .text()
+      .trim();
+  
+    var date = $(this)
+      .find("span")
+      .text()
+      .trim();
+  
+      tempArr.push({
+        text: text,
+        date: date
+      });
+      
+    });
+    var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
 
+  // update array on tasks object and save
+  tasks[arrName] = tempArr;
+  saveTasks();
+    console.log(tempArr);
+  }
+});
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    // remove dragged element from the dom
+    ui.draggable.remove();
+
+  },
+  over: function(event, ui) {
+    console.log(ui);
+  },
+  out: function(event, ui) {
+    console.log(ui);
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
